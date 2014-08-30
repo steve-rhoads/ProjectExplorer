35c143a20ad3fe0d47054e73297daa2b404f9b91
Steve Rhoads <steve_rhoads@cable.comcast.com>
2014-08-30 11:36:36 +0000

    Added iteration combobox with xtype

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-    items:{ html:'This is version 2'},
+    items:[{ html:'This is version 2'},
+    {xtype: 'rallyiterationcombobox'}],
     launch: function() {
         console.log("Console-version 2");
     }
